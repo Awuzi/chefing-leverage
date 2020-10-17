8733296489a1fd9670a1a5ed972bc5b461efa3cd
Yahia <yahia.lamri@outlook.fr>
2020-10-17 14:57:50 +0200

    add base config

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import Home from "./src/screens/Home";
+import List from "./src/screens/List";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to Chefing Levrage (ici la page de connexion avec google)</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const AppNavigator = createStackNavigator(
+  {
+    Home,
+    List,
   },
-});
+  {
+    initialRouteName: "Home",
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
